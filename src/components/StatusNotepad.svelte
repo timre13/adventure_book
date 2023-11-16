324c1dc4f03784d127c5ae097f6a8feb6fd9e32a
timre13 <itorteli13@gmail.com>
2023-11-16 21:25:31 +0100

    Start working on status area

---
+++ src/components/StatusNotepad.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    class Stat {
+        name: string = "";
+        value: number = 0;
+        maxValue: number | undefined = undefined;
+
+        constructor(name: string, value: number, maxValue: number | undefined) {
+            this.name = name;
+            this.value = value;
+            this.maxValue = maxValue;
+        }
+    }
+
+    export let stats: Array<Stat>;
+    export let inventory: Object;
+</script>
+
+<div class="status-notepad">
+    <div id="stat-display">
+        {#each stats as stat}
+            {#if stat.maxValue != undefined}
+                <p>TODO</p>
+            {:else}
+                <p>{stat.name}: {stat.value}</p>
+            {/if}
+        {/each}
+    </div>
+    <div id="inventory-display">
+        <p>TODO</p>
+    </div>
+</div>
