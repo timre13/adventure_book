d9b39692419e4ea84927cf7539930d01bf97333f
mikobalint-szbi <miko.balint.miklos@diak.szbi-pg.hu>
2023-11-16 17:10:23 +0100

    Experimenting with the eval function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export class Memory {
+    //static strings: { [id: string] : string; } = {"A":"B"};
+    //static numbers: { [id: string] : string; } = {};
+    //static bools: { [id: string] : string; } = {};
+    //static lists: { [id: string] : []; } = {};
+    // listák: id="listaNeve:0", id="listaNeve:1", stb
+    static variables: { [id: string]: any } = {};
+}
+export let vars: { [id: string]: any } = {};
+
+export function conditionHandler(conString: string): void {
+    console.log(eval(conString));
+
+
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {conditionHandler, vars} from "$lib/index"
 
-<div />
+    conditionHandler("vars['B'] = 'A'") // létrehozok egy B változót A értékkel
+    conditionHandler("vars['B'] == 'A'") // True értéket ad vissza
+
+
+</script>
+
+
+
