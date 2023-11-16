8d98336e72733b82d749fbc0fe10b5d731be5b44
timre13 <itorteli13@gmail.com>
2023-11-16 13:17:48 +0100

    Basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
 
-<div />
+<main>
+    <div id="left-panel" />
+    <div id="center-panel" />
+    <div id="right-panel" />
+</main>
+
+<style lang="scss">
+    :global(*) {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    main {
+        display: grid;
+        grid-template-columns: 1fr 2fr 1fr;
+        height: 100vh;
+        background-color: #e4d9bb;
+
+        #left-panel {
+        }
+
+        #center-panel {
+            border: 2px solid black;
+            height: 100%;
+        }
+
+        #right-panel {
+        }
+    }
+</style>
