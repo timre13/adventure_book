309cac5d05c6ec8523c5dcc2a0310125549b18a3
timre13 <itorteli13@gmail.com>
2023-11-17 09:36:25 +0100

    Implement status notepad

--- src/components/StatusNotepad.svelte
+++ src/components/StatusNotepad.svelte
@@ -1,31 +1,61 @@
 <script lang="ts">
-    class Stat {
-        name: string = "";
-        value: number = 0;
-        maxValue: number | undefined = undefined;
-
-        constructor(name: string, value: number, maxValue: number | undefined) {
-            this.name = name;
-            this.value = value;
-            this.maxValue = maxValue;
-        }
-    }
+    import type { Stat } from "$lib/status";
 
     export let stats: Array<Stat>;
-    export let inventory: Object;
+    export let inventory: Record<string, number>;
 </script>
 
+<!-- svelte-ignore a11y-missing-attribute -->
 <div class="status-notepad">
-    <div id="stat-display">
-        {#each stats as stat}
-            {#if stat.maxValue != undefined}
-                <p>TODO</p>
-            {:else}
-                <p>{stat.name}: {stat.value}</p>
-            {/if}
-        {/each}
-    </div>
-    <div id="inventory-display">
-        <p>TODO</p>
+    <img src="/notepad.png" />
+    <div class="content">
+        <div id="stat-display">
+            <p><b>Státusz</b></p>
+            {#each stats as stat}
+                {#if stat.maxValue}
+                    <p>{stat.name}: {stat.value}/{stat.maxValue}</p>
+                {:else}
+                    <p>{stat.name}: {stat.value}</p>
+                {/if}
+            {/each}
+        </div>
+        <div id="inventory-display">
+            <p><b>Tárgyak</b></p>
+            {#each Object.entries(inventory) as [item, cnt]}
+                <p>{item}: {cnt} db</p>
+            {/each}
+        </div>
     </div>
 </div>
+
+<style lang="scss">
+    @import url("https://fonts.googleapis.com/css2?family=Caveat&display=swap");
+
+    .status-notepad {
+        position: relative;
+        width: 480px;
+        height: 620px;
+        align-self: center;
+        font-family: "Caveat", cursive;
+        font-size: 26px;
+
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+
+        .content {
+            position: absolute;
+            left: 20px;
+            top: 49px;
+            background-color: rgb(204, 201, 181);
+            width: 451px;
+            height: 561px;
+            padding: 1rem;
+            display: flex;
+            flex-direction: column;
+            gap: 1.5rem;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     import { conditionHandler, vars } from "$lib/index";
+    import StatusNotepad from "../components/StatusNotepad.svelte";
+    import { Stat } from "$lib/status";
 
     conditionHandler("vars['B'] = 'A'"); // létrehozok egy B változót A értékkel
     conditionHandler("vars['B'] == 'A'"); // True értéket ad vissza
 
     //let parser = new DOMParser();
     //let xmlDoc = parser.parseFromString("<game><pages><page/><page/></pages></game>", "text/xml");
+
+    let stats: Array<Stat> = [new Stat("Életerő", 85, 100), new Stat("Szerencse", 40), new Stat("Ügyesség", 22)];
+    let inventory: Record<string, number> = { Alma: 12, Kulcs: 3, Kard: 1 };
 </script>
 
 <main>
     <div id="left-panel" />
     <div id="center-panel" />
-    <div id="right-panel" />
+    <div id="right-panel">
+        <StatusNotepad {stats} {inventory} />
+    </div>
 </main>
 
 <style lang="scss">
@@ -30,6 +37,8 @@
         }
 
         #right-panel {
+            display: flex;
+            flex-direction: column;
         }
     }
 </style>
