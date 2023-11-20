572de98ace85a8fa624e36c7579078480d6565f3
mikobalint <92861159+mikobalint@users.noreply.github.com>
2023-11-20 17:48:21 +0100

    Designing XML layout

--- src/lib/page.ts
+++ src/lib/page.ts
@@ -1,11 +1,11 @@
 export class Button {
-    constructor(public text: string) { }
+    constructor(public text: string) {}
 }
 
 export class Enemy {
-    constructor(public name: string) { }
+    constructor(public name: string) {}
 }
 
 export class Page {
-    constructor(public text: string, public enemies: Array<Enemy>, public buttons: Array<Button>) { }
+    constructor(public text: string, public enemies: Array<Enemy>, public buttons: Array<Button>) {}
 }
